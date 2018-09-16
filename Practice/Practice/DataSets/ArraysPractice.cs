4e0e3e0046589b65a5e8146f0fc4348338b2fc4e
Arthur Tsvettsih <arthur.tsvettsih@gmail.com>
2018-09-16 12:01:20 +0100

    Added Arrays, ArrayLists and Lists
    Create a menu draft

---
+++ Practice/Practice/DataSets/ArraysPractice.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Resources:
+// https://msdn.microsoft.com/en-us/library/ms379570(v=vs.80).aspx 
+
+namespace Practice.DataSets
+{
+    class ArraysPractice
+    {
+        public static void HowToCreateAndResizeLists()
+        {
+            List<string> listExampleEmpty = new List<string>();
+            listExampleEmpty.Add("Hello");
+            listExampleEmpty.Add("World");
+
+            List<int> listExample = new List<int> { 1, 2 };
+
+            List<string> listExampleWithInitialSize = new List<string>(100);
+            listExampleWithInitialSize[99] = "Hey";
+        }
+
+        public static void HowToCreateAndResizeArrayLists()
+        {
+            ArrayList arrayListExample = new ArrayList();
+
+            arrayListExample.Add(1);
+            arrayListExample.Add("");
+            arrayListExample.Add(new Guid()); // Being able to add every time into the ArrayList would require the developer to check
+                                              // the type of all objects before performing operations on them
+        }
+
+        public static void HowToCreateAnArray()
+        {
+            int[] arrayExampleEmpty = new int[3]; // Create an empty array of size 3
+            arrayExampleEmpty[0] = 1; // Overwrite default values of 0 with the appropriate values 
+            arrayExampleEmpty[1] = 2;
+            arrayExampleEmpty[2] = 3;
+
+            int[] arrayExample = { 1, 2, 3, 4, 5 }; // Create a populated array of size 5
+        }
+
+        public static void HowToResizeAnArray()
+        {
+            int[] arrayExample = { 1, 2, 3, 4, 5 };
+
+            int[] temp = new int[20]; // Create an empty temp array of the required size 
+            arrayExample.CopyTo(temp, 0); // Copy all elements from existing array to the new one
+            arrayExample = temp; // Assing temp array to the initial array
+        }
+    }
+}
--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -3,15 +3,127 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Practice.DataSets;
 
 namespace Practice
 {
     class Program
     {
+        //TODO: Extract everything from static Main to a non static Menu class, which will create the menu from the constructor
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            HandleCategoryMenu();
+            Console.ReadKey(); // Stop the application from closing at the end
+        }
 
+        private static void HandleCategoryMenu()
+        {
+            Console.WriteLine("Please select a category: ");
+            Console.WriteLine("1) Data Structures");
+
+            var input = GetUserMenuSelection();
+
+            switch (input)
+            {
+                case 1:
+                    {
+                        HandleDataStructureMenu();
+                        break;
+                    }
+                default:
+                    {
+                        HandleMenuDefaultCase();
+                        break;
+                    }
+            }
+
+        }
+
+        //TODO: Add input checks
+        private static int GetUserMenuSelection()
+        {
+            var rawInput = Console.ReadKey().KeyChar;
+            var input = Int32.Parse(rawInput.ToString());
+
+            PrintEmptyLines(2);
+
+            return input;
+        }
+
+        private static void PrintEmptyLines(int numberOfLines)
+        {
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                Console.WriteLine();
+            }
+        }
+
+        private static void HandleDataStructureMenu()
+        {
+            Console.WriteLine("Please select a topic: ");
+            Console.WriteLine("1) Arrays");
+
+            var input = GetUserMenuSelection();
+
+            switch (input)
+            {
+                case 1:
+                    {
+                        HandleArrayMenu();
+                        break;
+                    }
+                default:
+                    {
+                        HandleMenuDefaultCase();
+                        break;
+                    }
+            }
+        }
+
+        private static void HandleArrayMenu()
+        {
+            //TODO: Use reflection to get the list of arrays
+            Console.WriteLine("Please select a topic: ");
+            Console.WriteLine("1) How To Create An Array");
+            Console.WriteLine("2) How To Resize An Array");
+            Console.WriteLine("3) How To Create And Resize Array Lists");
+            Console.WriteLine("4) How To Create And Resize Lists");
+
+            var input = GetUserMenuSelection();
+
+            switch (input)
+            {
+                case 1:
+                    {
+                        ArraysPractice.HowToCreateAnArray();
+                        break;
+                    }
+                case 2:
+                    {
+                        ArraysPractice.HowToResizeAnArray();
+                        break;
+                    }
+                case 3:
+                    {
+                        ArraysPractice.HowToCreateAndResizeArrayLists();
+                        break;
+                    }
+                case 4:
+                    {
+                        ArraysPractice.HowToCreateAndResizeLists();
+                        break;
+                    }
+                default:
+                    {
+                        HandleMenuDefaultCase();
+                        break;
+                    }
+            }
+        }
+
+        private static void HandleMenuDefaultCase()
+        {
+            Console.WriteLine("Restart the application and enter a valid number");
             Console.ReadKey();
         }
     }
